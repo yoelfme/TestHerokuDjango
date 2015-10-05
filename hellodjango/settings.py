c1b0e4b32a8d6c1a4e3da67f77c11755f861c445
Yoel Monzon <yoelfme@hotmail.com>
2015-10-05 12:27:15 -0600

    [fix] min

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -74,12 +74,10 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+    }
+}
 
 
 # Internationalization
